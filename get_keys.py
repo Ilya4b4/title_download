240e0c83254a36481185bab242e8231e3c2e357d
Philipp Seiler <p.seiler@linuxmail.org>
2017-05-11 17:39:06 +0200

    added more additional information

--- get_keys.py
+++ get_keys.py
@@ -3,7 +3,7 @@ from urllib2 import urlopen
 import json, unicodedata
 
 
-url='https://wiiu.titlekeys.com/json'
+url='http://wiiu.titlekeys.gq/json'
 response = urlopen(url)
 parsed = json.load(response)
 key_file = open("keys.txt",'w') 
@@ -13,6 +13,7 @@ D7B00402659BA2ABD2CB0DB27FA2B656 # Wii U Common Key:
 B5D8AB06ED7F6CFC529F2CE1B4EA32FD # Wii U Starbuck Ancast Key
 ############################################################
 
+TitleID # TitleKey # Title Name (REGION) # Ticket Type
 """)
 data=''
 for i in xrange(len(parsed)):
@@ -20,11 +21,27 @@ for i in xrange(len(parsed)):
         pass
     else:
         key = parsed[i]['titleKey']
+        titleid = parsed[i]['titleID']
+        if titleid[:8] == "00050000":
+            tiktype = "eShop/Application";
+            pass
+        elif titleid[:8] == "00050002":
+            tiktype = "Demo"
+            pass
+        elif titleid[:8] == "0005000E":
+            tiktype = "Patch"
+            pass
+        elif titleid[:8] == "0005000C":
+            tiktype = "DLC"
+            pass
+        else:
+            tiktype = "Unknown"
+            pass
         name = parsed[i]['name']
         name = name.replace('\n','').replace('\t','')
         name = unicodedata.normalize('NFKD', name).encode('ascii','ignore')
         region = parsed[i]['region']
-        line_data = str(key),' # ',name,' (',region,')'
+        line_data = str(titleid),' # ', str(key),' # ',name,' (',region,')',' # ',str(tiktype)
         normalized_data = "".join(line_data)
         key_file.write("%s\n" %normalized_data)
 key_file.close()
