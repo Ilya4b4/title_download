0abebbc7cbf8a2322978ba45d17dfe47d05e5050
Philipp Seiler <p.seiler@linuxmail.org>
2017-05-18 16:53:52 +0200

    added getopts for parameters added help page updated README

--- README.md
+++ README.md
@@ -1,14 +1,40 @@
-# WiiU Title ID and Ticket Downloader
-Download pretty much all content from wiiu.titlekeys.gq
+# WiiU/3DS Title ID,Keys and Ticket Downloader
+Download pretty much all content from both wiiu.titlekeys.gq
+and 3ds.titlekys.gq
 * JSON file to setup a mirror from the original site
 * plain text key.txt for daily usage
-* $TITLEID.tik files for all availble ticket files
+* $TITLEID.tik files for all availble wiiu ticket files
+* decrypted and encrypted 3ds ticket files for use with freeshop etc
+
+## Arguments
+* -w, --wiiu     download everything from wiiu site"
+* -3, --3ds      download everything from 3ds site"
+* --wurl URL     specify url for the wiiu titlekey site with http[s] prefix"
+* --3url URL     specify url for the 3ds titlekey site with http[s] prefix"
+* -l, --local    generate key file only. Do not attempt for network connection"
 
 ## Usage
 ```bash
-python title_download.py
+python title:download.py -w -d
 ```
+Downloads everything from both 3ds and wiiu titlekey site
+
+```bash
+python title_download.py -3 --3url "http://example.site"
+```
+Downloads everything from 3ds titlekey site with a custom url
+
+```bash
+python title_download.py -w -3 --wurl "http://example.site" --3url "http://3ds.example.site"
+```
+Downloads everything from bot 3ds and wiiu titlkey site with custom urls
+for both arguments
+
+```bash
+python title_download.py -3 -w --local
+```
+Only converts the local "wiiu.json" and "3ds.json" file into human
+readable format
 
 ## ToDos
-* add working parameters to script
-* add suport for download titlekeys.bin, seeddb.bin and so on
+* none at the moment
--- title_download.py
+++ title_download.py
@@ -2,45 +2,37 @@
 from urllib2 import urlopen
 import json, unicodedata
 import os
-
-# setting workding directories
-script_dir = os.path.dirname(os.path.abspath(__file__))
-ticket_dir = os.path.join(script_dir, 'wiiu_tickets')
-bin_dir = os.path.join(script_dir, '3ds_files')
-try:
-        os.makedirs(ticket_dir)
-except OSError:
-        pass # already exists
-try:
-        os.makedirs(bin_dir)
-except OSError:
-        pass # already exists
+import sys
+import getopt
 
 #download URLs
+url_wiiu = 'http://wiiu.titlekeys.gq'
+url_3ds = 'http://3ds.titlekeys.gq'
+script_name = "titlekey_download.py"
+use_3ds = "no"
+use_wiiu = "no"
+local_only = "no"
 
-url_wiiu='http://wiiu.titlekeys.gq'
-url_3ds='http://3ds.titlekeys.gq'
+def usage():
+    print "usage: titlekey_download.py [OPTIONS]"
+    print "Arguments: "
+    print "   -w, --wiiu     download everything from wiiu site"
+    print "   -3, --3ds      download everything from 3ds site"
+    print "   --wurl URL     specify url for the wiiu titlekey site with http[s] prefix"
+    print "   --3url URL     specify url for the 3ds titlekey site with http[s] prefix"
+    print "   -l, --local    generate key file only. Do not attempt for network connection"
+    print "examples: "
+    print "   %s -w -d" % script_name
+    print "   Downloads everything from both 3ds and wiiu titlekey site\n"
+    print "   %s -3 --3url \"http://example.site\"" % script_name
+    print "   Downloads everything from 3ds titlekey site with a custom url\n"
+    print "   %s -w -3 --wurl \"http://example.site\" --3url \"http://3ds.example.site\""
+    print "   Downloads everything from bot 3ds and wiiu titlkey site with custom urls"
+    print "   for both arguments\n"
+    print "   %s -3 -w --local" % script_name
+    print "   Only converts the local \"wiiu.json\" and \"3ds.json\" file into human"
+    print "   readable format"
 
-# download JSON files from wiiu and 3ds from webserver and save it to "$CONSOLE.json"
-response = urlopen("%s/json" % url_wiiu)
-with open('wiiu.json', 'w') as json_file:
-    for item in response:
-          json_file.write("%s\n" % item)
-json_file.close()
-response = urlopen("%s/json" % url_3ds)
-with open('3ds.json', 'w') as json_file:
-    for item in response:
-          json_file.write("%s\n" % item)
-json_file.close()
-# read Content of $CONSOLE.json file and parse it from JSON syntax
-with open('wiiu.json') as json_file:
-    parsed_wiiu = json.load(json_file)
-json_file.close()
-with open('3ds.json') as json_file:
-    parsed_3ds = json.load(json_file)
-json_file.close()
-
-#open formatted output key.file
 def generate_keyfile(input,type):
     if type == "wiiu":
         key_file = open("keys_%s.txt" % type,'w')
@@ -56,18 +48,19 @@ TitleID # TitleKey # Title Name (REGION) # Ticket Type
         key_file = open("keys_%s.txt" % type,'w')
         key_file.write("""TitleID # TitleKey # encTitleKey # Title Name (REGION) # Serial # Ticket Type
 """)
-        remote_file_list = ["download","downloadenc","seeddb","downloadmissingforencryption"]
-        filename = ["decTitleKeys.bin","encTitleKeys.bin","seeddb.bin","decTitleKeys_missing.bin"]
-        count = 0
-        for i in remote_file_list:
-            url = "%s/%s" % (url_3ds, i)
-            file = urlopen(url)
-            path = os.path.join(bin_dir, filename[count])
-            print "Downloading:", path, ""
-            with open(path,'wb') as output:
-                  output.write(file.read())
-            file.close()
-            count = count + 1
+        if local_only == "no":
+            remote_file_list = ["download","downloadenc","seeddb","downloadmissingforencryption"]
+            filename = ["decTitleKeys.bin","encTitleKeys.bin","seeddb.bin","decTitleKeys_missing.bin"]
+            count = 0
+            for i in remote_file_list:
+                url = "%s/%s" % (url_3ds, i)
+                file = urlopen(url)
+                path = os.path.join(bin_dir, filename[count])
+                with open(path,'wb') as output:
+                      output.write(file.read())
+                file.close()
+                print "Downloaded: \"%s\"" % path, ""
+                count = count + 1
     for i in xrange(len(input)):
         if input[i]['titleKey'] == None or input[i]['name'] == None:
             pass
@@ -104,16 +97,17 @@ TitleID # TitleKey # Title Name (REGION) # Ticket Type
                 else:
                     tiktype = "Unknown"
                     pass
-                if input[i]['ticket'] == "1":
-                    filename = "%s.tik" % input[i]['titleID']
-                    path = os.path.join(ticket_dir, filename)
-                    url = "%s/ticket/%s" % (url_wiiu, filename)
-                    if not os.path.isfile(path):
-                        print "Downloading:", path
-                        ticketfile = urlopen(url)
-                        with open(path,'wb') as output:
-                              output.write(ticketfile.read())
-                        ticketfile.close()
+                if local_only == "no":
+                    if input[i]['ticket'] == "1":
+                        filename = "%s.tik" % input[i]['titleID']
+                        path = os.path.join(ticket_dir, filename)
+                        url = "%s/ticket/%s" % (url_wiiu, filename)
+                        if not os.path.isfile(path):
+                            ticketfile = urlopen(url)
+                            with open(path,'wb') as output:
+                                  output.write(ticketfile.read())
+                            ticketfile.close()
+                            print "Downloaded: \"%s\"" % path
             elif type == "3ds":
                 if titleid[:8] == "00040010" or "0004001b" or "0004009b" or "000400db" or "00040030" or "00040130" or "00040138":
                     tiktype = "System";
@@ -151,6 +145,78 @@ TitleID # TitleKey # Title Name (REGION) # Ticket Type
             line_data = "%s # %s" % (line_data, str(tiktype))
             normalized_data = "".join(line_data)
             key_file.write("%s\n" %normalized_data)
+    print "Created \"key_%s.txt\"\n" % type
 
-generate_keyfile(parsed_wiiu, "wiiu")
-generate_keyfile(parsed_3ds, "3ds")
+
+
+
+# define parameters
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'w3hl', ['wiiu', '3ds', 'help','wurl=','3url=', 'local'])
+except getopt.GetoptError:
+    print "Wrong arguments given."
+    usage()
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt in ('-h', '--help'):
+        usage()
+        sys.exit(2)
+    elif opt in ('-w', '--wiiu'):
+        use_wiiu = "yes"
+    elif opt in ('-3', '--3ds'):
+        use_3ds = "yes"
+    elif opt in ('-l', '--local'):
+        local_only = "yes"
+    elif opt in ('--wurl'):
+        url_wiiu = arg
+    elif opt in ('--3url'):
+        url_3ds = arg
+    else:
+        print "No arguments given"
+        usage()
+        sys.exit(2)
+
+if (use_wiiu == "no" and use_3ds == "no"):
+    print "No arguments given. Please use at least -w or -3\n"
+    usage()
+    sys.exit(2)
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+if use_3ds == "yes":
+    if local_only == "no":
+        bin_dir = os.path.join(script_dir, '3ds_files')
+        try:
+            os.makedirs(bin_dir)
+        except OSError:
+            pass # already exists
+        response = urlopen("%s/json" % url_3ds)
+        with open('3ds.json', 'w') as json_file:
+            for item in response:
+                json_file.write("%s\n" % item)
+        json_file.close()
+        print "Downloaded \"3ds.json\""
+    with open('3ds.json') as json_file:
+        parsed_3ds = json.load(json_file)
+    json_file.close()
+    generate_keyfile(parsed_3ds, "3ds")
+if use_wiiu == "yes":
+    if local_only == "no":
+        ticket_dir = os.path.join(script_dir, 'wiiu_tickets')
+        try:
+            os.makedirs(ticket_dir)
+        except OSError:
+            pass # already exists
+# download JSON files from wiiu and 3ds from webserver and save it to "$CONSOLE.json"
+        response = urlopen("%s/json" % url_wiiu)
+        with open('wiiu.json', 'w') as json_file:
+            for item in response:
+                  json_file.write("%s\n" % item)
+        json_file.close()
+        print "Downloaded \"wiiu.json\""
+# read Content of $CONSOLE.json file and parse it from JSON syntax
+    with open('wiiu.json') as json_file:
+        parsed_wiiu = json.load(json_file)
+    json_file.close()
+    generate_keyfile(parsed_wiiu, "wiiu")
+
